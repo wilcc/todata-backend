7cfa9da1f06191e6599460f87102044973d223e3
Wilson Chen <wilson.chen@codeimmersives.com>
2020-03-26 16:57:03 -0400

    finish

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
-const getTodoName = function() {}
-const getCompleteness = function() {}
-const getPriority = function() {}
-const isComplete = function() {}
-const isHighPriority = function() {}
-
+const getTodoName = function(obj) {
+  return obj.text
+}
+const getPriority = function(obj) {
+  return obj.priority
+}
+const isComplete = function(obj) {
+  return obj.complete
+}
+const isHighPriority = function(obj) {
+  return obj.priority > 1
+}
+const ishigh = function(obj) {
+  if (obj.priority > 1){
+    return 'High'
+  } else return 'Low'
+}
+const nameAndPriority = function(obj){
+  return getTodoName(obj) + ' - ' + ishigh(obj)
+}
+const isNotComplete = function(obj){
+  return !obj.complete
+}
+const notHighPriorities = function(obj) {
+  return obj.priority < 2
+}
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
-const names = function() {}
-const priorities = function() {}
-const namesAndPriorities = function() {}
-const justNotComplete = function() {}
-const justComplete = function() {}
-const priority2Only = function() {}
-const priority1Only = function() {}
+const names = function(array) {
+  return array.map(getTodoName)
+}
+const priorities = function(array) {
+  return array.map(getPriority)
+}
+const namesAndPriorities = function(array) {
+  return array.map(nameAndPriority)
+}
+const justNotComplete = function(array) {
+  return array.filter(isNotComplete)
+
+}
+const justComplete = function(array) {
+  return array.filter(isComplete)
+}
+const priority2Only = function(array) {
+  return array.filter(isHighPriority)
+}
+const priority1Only = function(array) {
+  return array.filter(notHighPriorities)
+}
 
 
 module.exports = {
   getTodoName,
-  getCompleteness,
   getPriority,
   isComplete,
   isHighPriority,
--- main.test.js
+++ main.test.js
@@ -12,7 +12,7 @@ const {
   priority1Only,
 } = require('./main.js')
 
-const todos = require('./todos.js')
+// const todos = require('./todos.js')
 
 let originalTodos = [];
 
@@ -317,3 +317,53 @@ describe('priority1Only', () => {
     expect(todos).toEqual(originalTodos);
   });
 })
+
+
+
+const todos = [
+  {
+    text: 'Christmas shopping',
+    id: 0,
+    complete: false,
+    priority: 1,
+  },
+  {
+    text: `make doctor's appointment`,
+    id: 1,
+    complete: true,
+    priority: 2,
+  },
+  {
+    text: 'binge watch The Office',
+    id: 2,
+    complete: false,
+    priority: 2,
+  },
+  {
+    text: 'research dinosaurs',
+    id: 3,
+    complete: false,
+    priority: 1,
+  },
+  {
+    text: 'fire Colin',
+    id: 4,
+    complete: true,
+    priority: 2,
+  },
+  {
+    text: 'start novel',
+    id: 5,
+    complete: true,
+    priority: 1,
+  },
+
+]
+
+let currentTodos = [...todos];
+
+let showingPriorities = false;
+
+if (!window) {
+  module.exports = todos;
+}
